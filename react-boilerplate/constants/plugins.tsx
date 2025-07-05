23e6c0359a487437882ba9affef09d53b4f9536d
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-07-05 21:43:02 +0200

    add plugins info

---
+++ react-boilerplate/constants/plugins.tsx
@@ -0,0 +1,28 @@
+export const plugins = [
+    {
+        title: "Hardhat 3.0 MCP Server",
+        info: "Created during the hackathon for key generation",
+        price: 6,
+        image: "/images/hardhat.png",
+    },
+    {
+        title: "DeepStego: Neural Network",
+        info: "Using LLMs and advanced Steganography architecture",
+        price: 4,
+        // image: "/images/deep.png",
+        image: "/images/deepstego.jpg",
+    },
+    {
+        title: "Integration Flow Blockchain",
+        info: "See how flow helps to store custom mnemonic phrases and secure payments",
+        price: 9,
+        image: "/images/flow.jpg",
+    },
+    {
+        title: "Mantle's Ecosystem Alignment",
+        info: "For undeniable use-case and exceptional experience",
+        price: 4,
+        // image: "/images/deep.png",
+        image: "/images/mantle.jpg",
+    },
+];
